f024ccd8973fbf3b3d1dadddd1ff842e610cc6e9
edtf491 <arturtutaev1@gmail.com>
2025-06-22 20:25:32 +0300

    Add files via upload

---
+++ git gui/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+#include "askname.h"
+
+int main(int argc, char** argv)
+{
+    char first[255], last[255];
+
+    askname(first, last);
+
+    printf("Hello, %s %s!\n", first, last);
+=======
+
+int main(int argc, char** argv)
+{
+    printf("Hello world!\n");
+>>>>>>> parent of e0d104a (Give a personal hello)
+=======
+
+int main(int argc, char** argv)
+{
+    printf("Hello world!\n");
+>>>>>>> parent of e0d104a (Give a personal hello)
+=======
+
+int main(int argc, char** argv)
+{
+    printf("Hello world!\n");
+>>>>>>> parent of e0d104a (Give a personal hello)
+    return 0;
+}
